0b3183916df0000f82eeca3e677ee66ea120ea15
Salih Kavaf <52260488+salihkavaf@users.noreply.github.com>
2021-09-21 11:14:50 +0300

    Fixed syntax.

--- lib/index.ts
+++ lib/index.ts
@@ -78,7 +78,9 @@ class Importer {
                 return;
 
             if (file.isBuffer()) {
-                const list = await that.iterateCache(file.path, async function (ref) { return await that.resolveBufferRef(ref) });
+                const list = await that.iterateCache(file.path, async ref => await that.resolveBufferRef(ref));
+                console.log("Length = " + list.length);
+                console.log(that._cache);
                 list.forEach(ref => this.push(ref));
             }
             else if (file.isStream()) {
